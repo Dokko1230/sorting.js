63476bbe141bb442b77d922452e491c79108eb1a
Sean Dokko <dokko1230@gmail.com>
2014-07-10 10:45:42 -0700

    adds pseudocode for mergesort

---
+++ mergesort/mergesort.js
@@ -0,0 +1,13 @@
+
+var mergesort = function(list) {
+  // partition the list into two array.
+  // make left and right lists
+  // add left side and right side
+  // assign left as the recursive call
+  // assign right as the recursive call
+  // Conquer and merge the two
+};
+
+var merge = function(list) {
+
+};
